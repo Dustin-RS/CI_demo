a8d1828b15b9129a83358b7a1320b7ed3b685fde
Dusin-RS <omirzak.d@phystech.edu>
2024-05-11 02:19:29 +0300

    Fixed linter

--- simple_code.py
+++ simple_code.py
@@ -1,12 +1,14 @@
-import numpy as np, pandas as pd
+"""Module provide simple data generation based on numpy and pandas"""
+import numpy as np
+import pandas as pd
 from IPython.display import display
 
-n = 10_000
-X = np.random.randn(n, 10)
+CONSTANT_N = 10_000
+X = np.random.randn(CONSTANT_N, 10)
 valuable_features = [0, 1, 4, 5, 8]
 
 # y = sign(f0 + f1 + f4 + f5 + f8)
-noise = np.random.randn(n) / 5
+noise = np.random.randn(CONSTANT_N) / 5
 y = ((X[:, valuable_features].sum(axis=1) + noise) > 0).astype(np.uint8)
 
 display(pd.Series(y).value_counts())
